fe8653b805b85969406be42e63e57740471ce0b3
Krishnendu Ghosh <24krishnendu@gmail.com>
2026-08-16 19:58:57 +0530

    Create 01_UserInputOutput.java

---
+++ Striver's A2Z DSA SheetPhase 01: Learn the basics/Sec 1: Things to Know in Java, C++ or any language/01_UserInputOutput.java
@@ -0,0 +1,26 @@
+/*
+    Problem: Input Output
+
+    Complete the program to take an integer input from the user and print the same value on the screen.
+
+    Example: Input: 7 → Output: 7
+
+    Constraint:
+    -1000 <= User Input <= 1000
+*/
+
+import java.util.Scanner;
+
+public class UserInputOutput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println(number);
+
+        sc.close();
+    }
+}
