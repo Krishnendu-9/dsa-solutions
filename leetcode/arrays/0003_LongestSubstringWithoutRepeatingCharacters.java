05bdce38156010be656fdf95004c22129856fdde
Krishnendu Ghosh <24krishnendu@gmail.com>
2026-08-16 20:54:14 +0530

    Add solution for longest substring without repeating characters
    Implement lengthOfLongestSubstring method to find the length of the longest substring without repeating characters.

---
+++ leetcode/arrays/0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, answer = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            int length = right - left + 1;
+          
+            if (length > answer) {
+                answer = length;
+            }
+        }
+        return answer;
+    }
+}
