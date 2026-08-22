9115733b0e383a94c336a4f0a1a805a98ce7462f
Krishnendu Ghosh <24krishnendu@gmail.com>
2026-08-22 19:03:06 +0530

    Create 3622_CheckDivisibilityByDigitSumAndProduct.java

---
+++ leetcode/math/3622_CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+
+        int num = n, sum = 0, product = 1;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            sum += digit;
+            product *= digit;
+            num /= 10;
+        }
+        return n % (sum + product) == 0;
+    }
+}
