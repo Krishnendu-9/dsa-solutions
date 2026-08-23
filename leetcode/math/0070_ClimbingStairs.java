be59147f7db41d1c52eb367a6fdd229b76aa0052
Krishnendu Ghosh <24krishnendu@gmail.com>
2026-08-23 13:32:04 +0530

    Implement Climbing Stairs solution in Java

---
+++ leetcode/math/0070_ClimbingStairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int first = 1, second = 2, next=0;
+
+        for (int i = 3; i <= n; i++) {
+
+            next = first + second;
+
+            first = second;
+            second = next;
+        }
+        return second;
+    }
+}
